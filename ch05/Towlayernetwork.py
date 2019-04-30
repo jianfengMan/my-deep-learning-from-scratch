d33d13ed583c9e9658552d94412a536662dc337e
jianfengMan <1762639951@qq.com>
2019-04-30 14:29:01 +0800

    add file

---
+++ ch05/Towlayernetwork.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+import sys, os
+
+sys.path.append(os.pardir)
+from common.functions import *
+from common.gradient import numerical_gradient
+
+
+class TwoLayerNet:
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['w1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['w2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        w1, w2 = self.params['w1'], self.params['w2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, w1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, w2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['w1'] = numerical_gradient(loss_W, self.params['w1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['w2'] = numerical_gradient(loss_W, self.params['w2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
